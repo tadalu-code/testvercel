bfd9b33598a15c55f18884d770071d6b8c9ed6f8
Dat <thanhdatlu454@gmail.com>
2026-07-27 00:38:04 +0700

    fix: update BASE_URL to use Vercel env vars

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 // ======================= GIAO DIỆN CHUNG =======================
-const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL 
+  || (process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : 
+     (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"));
 
 if (typeof window === 'undefined') {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
